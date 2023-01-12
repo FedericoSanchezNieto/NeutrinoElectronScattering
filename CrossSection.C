62cea9abb2efde726a27e1af79be9fb0db6b1869
Federico Sanchez <fsanchez@cern.ch>
2023-01-12 11:29:14 +0100

    First version of the code

---
+++ CrossSection.C
@@ -0,0 +1,107 @@
+#include "NeutrinoElectronScattering.h" 
+
+
+void CrossSection(double Enu = 10.){
+
+  TH1F *histang = new TH1F("histang","",1000,0.,3.141592/2);
+  TH1F *histanganu = new TH1F("histanganu","",1000,0.,3.141592/2.); 
+
+  TH1F *histEang = new TH1F("histEang","",1000,0.,1.);
+  TH1F *histEanganu = new TH1F("histEanganu","",1000,0.,1.); 
+
+  TH1F *histE = new TH1F("histE","",1000,0.,Enu);
+  TH1F *histEanu = new TH1F("histEanu","",1000,0.,Enu); 
+
+  TH1F *histEMC = new TH1F("histEMC","",1000,0.,Enu);
+  TH1F *histEMCanu = new TH1F("histEMCanu","",1000,0.,Enu);
+
+  TH2F *histEang2vsang = new TH2F("histEang2vsang"," ",100,0.,3.141592/2,500,0.,1.2); 
+  
+  
+  TRandom r;
+
+  NeutrinoElectronScattering nul; 
+
+  double xsecttot = nul.Totalcrosssection(Enu,neutrinoelectron);
+  double anuxsecttot = nul.Totalcrosssection(Enu,-neutrinoelectron);
+  
+  for(int i = 0; i < 1e+8; i++ ) {
+
+    double a,b;
+
+    nul.GENcrosssection(Enu,a,b,neutrinoelectron); 
+
+    histEMC->Fill(a,1./1.e+8); 
+
+    nul.GENcrosssection(Enu,a,b,-neutrinoelectron); 
+
+    histEMCanu->Fill(a,1./1.e+8); 
+    
+    double Ee  = r.Uniform(nul.electronmass(),Enu);
+    
+    double xs = nul.diffcrosssection(Enu,Ee,neutrinoelectron)/xsecttot/1.e+8;
+    double axs = nul.diffcrosssection(Enu,Ee,-neutrinoelectron)/anuxsecttot/1.e+8;
+
+    double cos = nul.GetCosine(Enu,Ee,neutrinoelectron);
+    double theta = acos(cos); 
+    double Etheta2 = Ee*theta*theta; 
+    //  if( abs(cos) > 1 ) continue;
+    
+    if(xs > 0 ) {
+      histEang->Fill(Etheta2,xs);
+      histE->Fill(Ee,xs);
+      histang->Fill(theta,xs);
+      histEang2vsang->Fill(theta,Etheta2,xs); 
+    }
+
+    if(axs > 0 ) {
+      histEanganu->Fill(Etheta2,axs);
+      histEanu->Fill(Ee,axs);
+      histanganu->Fill(theta,axs); 
+    }
+    
+  }
+
+
+  TH1F *histEnu = new TH1F("histEnu","",1000,0.,5000.);  
+
+  for(int i = 0;i < 1000;i++) {
+    double E = histEnu->GetBinCenter(i+1);
+    double xs = nul.Totalcrosssection(E,neutrinoelectron)*1.e-27;
+    histEnu->SetBinContent(i+1,xs);
+  }
+  
+  TCanvas *c1 = new TCanvas("c1","",800,800);
+  histEnu->Draw();
+  
+  c1->Update();
+
+  TCanvas *c2 = new TCanvas("c2","",800,800);
+  histEang2vsang->Draw("colz");
+  c2->Update();
+  
+  TCanvas *c = new TCanvas("c","",800,800);
+  c->Divide(2,2);
+  c->cd(1); 
+  histang->Draw("hist L");
+  histanganu->Draw("hist L same");
+  histanganu->SetLineColor(2);
+  c->cd(2);
+  histEanu->Draw("hist L");
+  histE->Draw("hist L same");
+  histEanu->SetLineColor(2);
+  histEMC->Draw("hist L same");
+  histEMC->SetLineColor(3);
+  histEMCanu->Draw("hist L same");
+  histEMCanu->SetLineColor(4);
+  c->cd(3);
+  histEang->Draw("hist L");
+  histEanganu->Draw("hist L same");
+  histEanganu->SetLineColor(2);
+  c->cd(4);
+  histEMCanu->Draw("hist");
+  histEMC->Draw("hist same");
+  c->Update();
+  
+}
+
---
+++ NeutrinoElectronScattering.h
@@ -0,0 +1,297 @@
+#define neutrinoelectron  12
+#define neutrinomuon      14
+#define neutrinotau       16
+
+#include <stdlib.h>
+#include <math.h>
+
+class NeutrinoElectronScattering {
+  
+ private:
+  double sin2w = 0.23121; 
+  double gv = -1./2.+2.*sin2w;
+  double ga = -1./2.;
+  double me = 0.510998950; // MeV
+  double mmu = 105.6583755; // MeV
+  double mtau = 1776.86; // MeV
+  
+  double Pi = 3.1415927;
+  double GF = 1.166378e-5; //  GeV^-2 
+  double GF2 =  GF*GF;
+  double MeV2mbarn = 1./2.56819e+6;   
+
+  
+ public:             // Access specifier
+  NeutrinoElectronScattering () {
+
+    std::cout<< " Neutrino-Electron scattering Initialised " << std::endl;
+  }
+
+  double electronmass(void) { return me;}
+  double muonmass(void) { return mmu;} 
+  double taumass(void) { return mtau;}
+  double S(double Enu) { return (Enu+electronmass())*(Enu+electronmass())-Enu*Enu;}
+  double Y(double Enu,double Ee) { return (Ee-electronmass())/Enu; }
+  double Normalization(void) { return GF2/Pi*MeV2mbarn;}
+  
+  double diffcrosssection(double Enu, double El, int neutrino){
+    bool aneut = false; 
+
+    if( neutrino < 0 ) aneut = true;  // It is anti-neutrino 
+
+    //  std::cout << abs(neutrino) << "  " <<  aneut << std::endl; 
+
+    double xs = 0;
+
+    if( El < electronmass() ) return 0; 
+    
+    if( abs(neutrino) ==  neutrinoelectron )
+      xs = nuee(Enu,El,aneut);
+    else
+      xs = nule(Enu,El,aneut); 
+
+    if( xs < 0 ) xs = 0;
+    
+    return xs; 
+  }
+
+  double GENcrosssection(double Enu, double &El,double &cos, int neutrino){
+    bool aneut = false; 
+    
+    if( neutrino < 0 ) aneut = true;  // It is anti-neutrino
+    
+    double xs = 0;
+
+    if( abs(neutrino) ==  neutrinoelectron )
+      xs = GENnuee(Enu,El,cos,aneut);
+    else
+      xs = GENnule(Enu,El,cos,aneut); 
+
+    //  if( aneut ) std::cout << xs << std::endl;
+    
+    if( xs < 0 ) xs = 0;
+
+    return xs;
+  }
+
+
+     
+  double Totalcrosssection(double Enu, int neutrino){
+    bool aneut = false; 
+
+    if( neutrino < 0 ) aneut = true;  // It is anti-neutrino 
+
+    double xs = 0;
+
+    if( abs(neutrino) ==  neutrinoelectron )
+      xs =  nueeInt(Enu,aneut);
+    else
+      xs = nuleInt(Enu,aneut); 
+
+    //  std::cout << Enu << "  " << xs << std::endl;
+    
+    if( xs < 0 ) xs = 0;
+
+    return xs;
+  }
+
+  
+  double GetCosine(double Enu,double El,int neutrino){
+
+    double mass;
+
+    if( abs(neutrino) == neutrinoelectron )
+      mass = me;
+    else if( abs(neutrino) == neutrinomuon )
+      mass = mmu;
+    else if( abs(neutrino) == neutrinotau )
+      mass = mtau;
+    else
+      mass = 1.e+32; 
+    
+    double Pl = sqrt(El*El-mass*mass); 
+   
+    return (El*mass+Enu*El-mass*mass-Enu*mass)/Enu/Pl;
+
+  }
+
+ private:
+  
+    double GENnule(double Enu, double &El,double &cos, bool aneut = false ){
+    
+    double norm = GF2/Pi*MeV2mbarn;
+
+    double s = S(Enu); 
+    
+    double CLL;
+    double CLR; 
+    
+    CLL = 1./2.+sin2w;
+    CLR = sin2w;
+    
+    double ymax = (Enu-me)/Enu;
+    
+    double N = nuleInt(Enu,aneut)/norm/s;
+
+    double y1 = GenerateY(CLL,CLR,N); 
+    
+    El = y1*Enu+me;
+
+    cos = GetCosine(Enu,El,neutrinoelectron);
+    
+    // std::cout << x1 << std::endl;
+    
+    return  nule( Enu,  El, aneut );
+    
+  }
+
+  
+  double GenerateY(double CLL,double CLR,double N) {
+    double r = (double)rand()/(double)RAND_MAX;
+    
+    double A = CLL*CLL/N;
+    double B = CLR*CLR/N;
+    double C = r; 
+
+    // x = 1-y 
+    // B/3 x^3 + A x + ( C - B/3 - A ) = 0
+    //  
+    //
+
+    double a2 = 3.*A/B;
+    double a3 = 3.*(C-B/3.-A)/B;
+
+    double Q = a2/3.;
+    double R = -a3/2.;
+    
+    double discriminant = Q*Q*Q+R*R;
+
+    double S1 = pow(R+sqrt(discriminant),1./3.);
+
+    double S2 = -pow(sqrt(discriminant)-R,1./3.); 
+
+    //    std::cout << discriminant << " S1= " << S1 << "  S2= " << S2 << std::endl; 
+
+    double x1 = S1+S2;
+
+    return 1-x1;
+
+ }
+
+    double GENnuee(double Enu, double &El,double &cos, bool aneut = false ){
+            
+    double CLL;
+    double CLR; 
+    
+    if( !aneut ) {
+      CLL = 1./2.+sin2w;
+      CLR = sin2w;
+    }
+    else {
+      CLL = -1./2.+sin2w;
+      CLR = 1./2.+sin2w;  
+    }
+
+    double ymax = (Enu-me)/Enu;
+    
+    double N = nueeInt(Enu,aneut)/Normalization()/S(Enu); 
+    
+    double y1 = GenerateY(CLL,CLR,N);  
+
+    El = y1*Enu+me;
+
+    cos = GetCosine(Enu,El,neutrinoelectron);
+    
+    //    if( aneut )  std::cout << y1 << std::endl;
+    
+    return   nuee( Enu,  El, aneut );
+    
+  }
+
+    
+
+  double nueeInt(double Enu, bool aneut = false ){
+        
+    double CLL;
+    double CLR; 
+    
+    if( !aneut ) {
+      CLL = 1./2.+sin2w;
+      CLR = sin2w;
+    }
+    else {
+      CLL = -1./2.+sin2w;
+      CLR = 1./2.+sin2w;  
+    }
+
+    double ymax = (Enu-me)/Enu;
+    
+    return Normalization()*S(Enu)*(CLL*CLL*ymax+CLR*CLR*1./3.*(-(1-ymax)*(1.-ymax)*(1-ymax)+1.)); 
+    
+  }
+  
+  double nule(double Enu, double Ee, bool aneut = false ){
+    
+    double y = Y(Enu,Ee);
+    
+    double CLL;
+    double CLR; 
+    
+    
+    CLL = 1./2.+sin2w;
+    CLR = sin2w;
+    
+    return Normalization()*S(Enu)*(CLL*CLL+CLR*CLR*(1-y)*(1-y)); 
+    
+  }
+
+  double nuleInt(double Enu, bool aneut = false ){
+    
+    double norm = GF2/Pi*MeV2mbarn;
+    
+    double s = S(Enu); 
+    
+    double CLL;
+    double CLR; 
+     
+    CLL = 1./2.+sin2w;
+    CLR = sin2w;
+
+
+    double ymax = (Enu-me)/Enu;
+    
+    return norm*s*(CLL*CLL*ymax+CLR*CLR*1./3.*(-(1-ymax)*(1.-ymax)*(1-ymax)+1.));
+  }
+
+  
+  double nuee(double Enu, double Ee, bool aneut = false ){
+    
+    double norm = GF2/Pi*MeV2mbarn;
+
+    double s = S(Enu); 
+    
+    double y = Y(Enu,Ee); 
+    
+    double CLL;
+    double CLR; 
+    
+    if( !aneut ) {
+      CLL = 1./2.+sin2w;
+      CLR = sin2w;
+    }
+    else {
+      CLL = -1./2.+sin2w;
+      CLR = 1./2.+sin2w;  
+    }
+    
+    return norm*s*(CLL*CLL+CLR*CLR*(1-y)*(1-y)); 
+    
+  }
+
+
+  
+};
+
+
+
+
