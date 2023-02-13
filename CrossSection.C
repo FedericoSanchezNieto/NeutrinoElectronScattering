4912219f4ab9e651eaae0303e2be1b20cd452d4b
Federico Sanchez <fsanchez@cern.ch>
2023-02-13 15:41:50 +0100

    Add energy range for negative value

--- CrossSection.C
+++ CrossSection.C
@@ -1,7 +1,7 @@
 #include "NeutrinoElectronScattering.h" 
 
 
-void CrossSection(double Enu = 10.){
+void CrossSection(double EnuIn = 10.){
 
   TH1F *histang = new TH1F("histang","",1000,0.,3.141592/2);
   TH1F *histanganu = new TH1F("histanganu","",1000,0.,3.141592/2.); 
@@ -9,26 +9,35 @@ void CrossSection(double Enu = 10.){
   TH1F *histEang = new TH1F("histEang","",1000,0.,1.);
   TH1F *histEanganu = new TH1F("histEanganu","",1000,0.,1.); 
 
-  TH1F *histE = new TH1F("histE","",1000,0.,Enu);
-  TH1F *histEanu = new TH1F("histEanu","",1000,0.,Enu); 
+  TH1F *histE = new TH1F("histE","",1000,0.,TMath::Abs(EnuIn));
+  TH1F *histEanu = new TH1F("histEanu","",1000,0.,TMath::Abs(EnuIn)); 
 
-  TH1F *histEMC = new TH1F("histEMC","",1000,0.,Enu);
-  TH1F *histEMCanu = new TH1F("histEMCanu","",1000,0.,Enu);
+  TH1F *histEMC = new TH1F("histEMC","",1000,0.,TMath::Abs(EnuIn));
+  TH1F *histEMCanu = new TH1F("histEMCanu","",1000,0.,TMath::Abs(EnuIn));
 
   TH2F *histEang2vsang = new TH2F("histEang2vsang"," ",100,0.,3.141592/2,500,0.,1.2); 
-  
+
+  TH2F *histEvsang = new TH2F("histEvsang"," ",100,0.,3.141592/2,100,0.,TMath::Abs(EnuIn)); 
   
   TRandom r;
 
   NeutrinoElectronScattering nul; 
 
-  double xsecttot = nul.Totalcrosssection(Enu,neutrinoelectron);
-  double anuxsecttot = nul.Totalcrosssection(Enu,-neutrinoelectron);
+  double Enu;
   
-  for(int i = 0; i < 1e+8; i++ ) {
+  for(int i = 0; i < 1e+7; i++ ) {
 
     double a,b;
 
+    if( EnuIn < 0 )
+      Enu = r.Uniform(0.,-EnuIn);
+    else
+      Enu = EnuIn;
+	
+    double xsecttot = nul.Totalcrosssection(Enu,neutrinoelectron);
+    double anuxsecttot = nul.Totalcrosssection(Enu,-neutrinoelectron);
+
+    
     nul.GENcrosssection(Enu,a,b,neutrinoelectron); 
 
     histEMC->Fill(a,1./1.e+8); 
@@ -51,7 +60,8 @@ void CrossSection(double Enu = 10.){
       histEang->Fill(Etheta2,xs);
       histE->Fill(Ee,xs);
       histang->Fill(theta,xs);
-      histEang2vsang->Fill(theta,Etheta2,xs); 
+      histEang2vsang->Fill(theta,Etheta2,xs);
+      histEvsang->Fill(theta,Ee,xs); 
     }
 
     if(axs > 0 ) {
@@ -102,6 +112,12 @@ void CrossSection(double Enu = 10.){
   histEMCanu->Draw("hist");
   histEMC->Draw("hist same");
   c->Update();
+
+  TCanvas *c3 = new TCanvas("c3","",800,800);
+  histEvsang->Draw("colz");
+  histEvsang->GetXaxis()->SetTitle("#theta_{e} (rads)");
+  histEvsang->GetYaxis()->SetTitle("E_{e} (MeV)"); 
+  c3->Update(); 
   
 }
 
